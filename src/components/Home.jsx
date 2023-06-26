a75d587e3f864781c3e45178bd3637b0a25ba4e4
LucasHeernan <lhbenitez2@gmail.com>
2023-06-26 13:15:28 -0300

    Responsive Home & NavBar in progress

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,46 +15,43 @@ export default function Home() {
   }, [])
 
   return (
-    <div className="w-full h-screen z-10 flex flex-col bg-violet-300"> {/* container home-page */}
-      <div className="mx-auto w-[85%] h-32 mt-40 bg-violet-700 sm:w-[95%] sm:h-36 sm:text-white md:w-4/5"> {/* text-zone */}
-        {/* <h1 className={`text-blue-900 font-['Alegreya'] font-bold ${start} lg:text-8xl md:text-6xl sm:text-5xl`}> */}
-        {/* <h1 className={`text-blue-900 font-['Alegreya_SC'] font-bold ${start} lg:text-8xl md:text-6xl sm:text-5xl`}> */}
-        {/* <h1 className={`text-blue-900 font-['Alegreya_Sans_SC'] font-bold ${start} lg:text-8xl md:text-6xl sm:text-5xl`}> */}
-        {/* <h1 className={`text-black font-['Philosopher'] font-bold ${start} lg:text-8xl md:text-6xl sm:text-5xl`}> */}
-        <h1 className={`text-[31px] font-['Poppins'] font-black ${start} bg-rose-500 sm:text-[44px] md:text-6xl lg:text-7xl`}>
-        {/* <h1 className={`text-black font-['Lexend_Deca'] font-bold ${start} lg:text-8xl md:text-6xl sm:text-5xl`}> */}
-        {/* <h1 className={`text-blue-900 font-['Besley'] font-bold ${start} lg:text-8xl md:text-6xl sm:text-5xl`}> */}
-        {/* <h1 className={`text-blue-900 font-['Libre_Bodoni'] font-bold ${start} lg:text-8xl md:text-6xl sm:text-5xl`}> */}
-        {/* <h1 className={`text-blue-900 font-['Bodoni_Moda'] font-bold ${start} lg:text-8xl md:text-6xl sm:text-5xl`}> */}
-        {/* <h1 className={`text-black font-['EB_Garamond'] font-bold ${start} lg:text-8xl md:text-6xl sm:text-5xl`}> */}
-        {/* <h1 className={`text-blue-900 font-['Cormorant_Garamond'] font-bold ${start} lg:text-8xl md:text-6xl sm:text-5xl`}> */}
-        {/* <h1 className={`text-blue-900 text-3xl font-['Sansita'] font-bold ${start} lg:text-8xl md:text-6xl sm:text-5xl`}> */}
+    <div className="w-full h-screen z-10 flex flex-col bg-violet-200"> {/* container home-page */}
+      <div className="mx-auto w-[88%] h-28 mt-40 sm:w-5/6 sm:h-32 md:h-40 lg:h-52 xl:h-64 xl:mt-32"> {/* text-zone */}
+      {/* <div className="mx-auto w-[88%] h-28 sm:w-5/6 sm:h-32 md:h-40 lg:h-52 xl:h-64"> text-zone */}
+        {/* <h1 className={`text-[28px] text-blue-900 font-['Alegreya'] font-bold ${start} sm:text-[39px] md:text-[46px] lg:text-[61px] xl:text-[77px]`}> */}
+        {/* <h1 className={`text-[28px] text-blue-900 font-['Alegreya_SC'] font-bold ${start} sm:text-[39px] md:text-[46px] lg:text-[61px] xl:text-[77px]`}> */}
+        {/* <h1 className={`text-[28px] text-blue-900 font-['Alegreya_Sans_SC'] font-bold ${start} sm:text-[39px] md:text-[46px] lg:text-[61px] xl:text-[77px]`}> */}
+        {/* <h1 className={`text-[28px] font-['Poppins'] font-black ${start} sm:text-[39px] md:text-[46px] lg:text-[61px] xl:text-[77px]`}> */}
+        {/* <h1 className={`text-[28px] text-black font-['Lexend_Deca'] font-bold ${start} sm:text-[39px] md:text-[46px] lg:text-[61px] xl:text-[77px]`}> */}
+        {/* <h1 className={`text-[28px] text-blue-900 font-['Besley'] font-bold ${start} sm:text-[39px] md:text-[46px] lg:text-[61px] xl:text-[77px]`}> */}
+        {/* <h1 className={`text-[28px] text-blue-900 font-['Libre_Bodoni'] font-bold ${start} sm:text-[39px] md:text-[46px] lg:text-[61px] xl:text-[77px]`}> */}
+        {/* <h1 className={`text-[28px] text-blue-900 font-['Bodoni_Moda'] font-bold ${start} sm:text-[39px] md:text-[46px] lg:text-[61px] xl:text-[77px]`}> */}
+        {/* <h1 className={`text-[28px] text-black font-['EB_Garamond'] font-bold ${start} sm:text-[39px] md:text-[46px] lg:text-[61px] xl:text-[77px]`}> */}
+        {/* <h1 className={`text-[28px] text-blue-900 font-['Cormorant_Garamond'] font-bold ${start} sm:text-[39px] md:text-[46px] lg:text-[61px] xl:text-[77px]`}> */}
+        <h1 className={`text-[33px] font-['Sansita'] font-extrabold ${start} sm:text-[44px] md:text-[53px] lg:text-[70px] xl:text-[89px]`}>
           <AnimatedLetters
             text={name}
             time={0}
           />
-        {/* </h1> */}
-        {/* <h1 className={`text-black font-['Alegreya'] font-bold ${start} lg:text-8xl md:text-6xl sm:text-5xl`}> */}
-        {/* <h1 className={`text-black font-['Alegreya_SC'] font-bold ${start} lg:text-8xl md:text-6xl sm:text-5xl`}> */}
-        {/* <h1 className={`text-black font-['Alegreya_Sans_SC'] font-bold ${start} lg:text-8xl md:text-6xl sm:text-5xl`}> */}
-        {/* <h1 className={`text-black font-['Philosopher'] font-bold ${start} lg:text-8xl md:text-6xl sm:text-5xl`}> */}
-        {/* <h1 className={`text-black font-['Poppins'] font-extrabold ${start} lg:text-7xl md:text-6xl sm:text-5xl`}> */}
-        {/* <h1 className={`text-black font-['Lexend_Deca'] font-bold ${start} lg:text-8xl md:text-6xl sm:text-5xl`}> */}
-        {/* <h1 className={`text-black font-['Besley'] font-bold ${start} lg:text-8xl md:text-6xl sm:text-5xl`}> */}
-        {/* <h1 className={`text-black font-['Libre_Bodoni'] font-bold ${start} lg:text-8xl md:text-6xl sm:text-5xl`}> */}
-        {/* <h1 className={`text-black font-['Bodoni_Moda'] font-bold ${start} lg:text-8xl md:text-6xl sm:text-5xl`}> */}
-        {/* <h1 className={`text-black font-['EB_Garamond'] font-bold ${start} lg:text-8xl md:text-6xl sm:text-5xl`}> */}
-        {/* <h1 className={`text-black font-['Cormorant_Garamond'] font-bold ${start} lg:text-8xl md:text-6xl sm:text-5xl`}> */}
-        {/* <h1 className={`text-black text-3xl font-['Sansita'] font-bold ${start} lg:text-8xl md:text-6xl sm:text-5xl`}> */}
           <AnimatedLetters
             text={work}
             time={21}
           />
         </h1>
       </div>
-      <h2 className="mx-auto w-[85%] text-lg text-black mt-2 font-['Alegreya_SC'] font-semibold bg-white sm:mt-6 sm:text-orange-500 md:text-lg">
-        Front End Developer / JavaScript Expert / Youtuber
+      <h2 className="mx-auto w-[88%] text-base text-black font-['Alegreya_SC'] font-medium sm:mt-6 sm:w-5/6 md:w-5/6 md:font-semibold lg:text-xl lg:w-5/6 xl:w-5/6">
+        Front End Developer - JavaScript Expert - Youtuber
       </h2>
+      <div className="mx-auto w-[88%] mt-7 bg-white">
+        <a href="#" className="relative mr-7 bg-transparent text-black px-3 py-1 rounded-md overflow-hidden border-[2px] border-black group hover:text-white duration-700">
+          <span className="absolute rounded-[4px] w-0 group-hover:w-[98%] transition-all ease-out duration-700 h-[95%] bg-black top-[1px] left-[1px]"></span>
+          <span className="relative uppercase font-semibold">Contacto</span>
+        </a>
+        <a href="#" className="relative bg-transparent text-black px-3 py-1 rounded-md overflow-hidden border-[2px] border-black group hover:text-white duration-700">
+          <span className="absolute w-0 rounded-[4px] group-hover:w-[99%] transition-all ease-out duration-700 h-[95%] bg-black top-[1px] right-[1px]"></span>
+          <span className="relative uppercase font-semibold">Descargar CV</span>
+        </a>
+      </div>
     </div>
   )
 }
