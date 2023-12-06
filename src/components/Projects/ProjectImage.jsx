6570992e1457e9dc6535e4c15f2eb0b8ed97e66d
LucasHeernan <lhbenitez2@gmail.com>
2023-12-06 11:43:55 -0300

    Fixed buttons

--- src/components/Projects/ProjectImage.jsx
+++ src/components/Projects/ProjectImage.jsx
@@ -20,8 +20,6 @@ const variants = {
 
 export default function ProjectImage({ images, phone }) {
 
-  const [left, setLeft] = useState(false);
-  const [right, setRight] = useState(false);
   const [arrowL, setArrowL] = useState(false);
   const [arrowR, setArrowR] = useState(false);
   const [currentImg, setCurrentImg] = useState(0);
@@ -54,28 +52,30 @@ export default function ProjectImage({ images, phone }) {
     <section className={`relative w-full mb-14 md:mb-20 ${phone ? "h-[480px] sm:w-4/6 sm:h-[490px] md:w-[60%] md:h-[510px] lg:w-[45%] lg:h-full" : "h-[315px] sm:w-[90%] sm:h-[380px] md:w-full md:h-[480px] lg:w-4/5 xl:w-1/2 xl:h-full" } before:w-full before:h-full before:absolute before:left-3 before:top-3 before:bg-slate-700 dark:before:bg-slate-950`}>
       <div className="relative z-10 overflow-hidden flex w-full h-full shadow-black shadow-md">
         <button
-          className={`absolute block w-1/4 h-full z-20 left-0 bg-gradient-to-l from-transparent to-slate-700 transition-opacity duration-700 ease-out opacity-[${left ? 0.5 : 0.1}]`}
+          className="absolute group block w-1/4 h-full z-20 left-0"
           onClick={prev}
-          onMouseEnter={() => setLeft(true)}
-          onMouseLeave={() => setLeft(false)}
         >
-          <em className={`absolute block h-7 w-7 top-1/2 left-7 transition-transform duration-200 ease-out delay-200
-          before:-rotate-${left ? "[25deg]" : 45} before:absolute before:left-0 before:h-full before:w-1 before:bottom-[1px] before:bg-black before:origin-top before:duration-200 before:ease-out
-          after:-rotate-${left ? "[155deg]" : "[135deg]"} after:absolute after:left-0 after:h-full after:w-1 after:bg-black after:origin-top after:duration-300 after:ease-out
-          ${arrowL ? "before:animate-leftDown before:bottom-0 after:animate-leftUp" : ""}`}
-          />
+          <div className="w-full h-full bg-gradient-to-l from-transparent to-slate-700 transition-opacity duration-500 ease-out opacity-10 group-hover:opacity-50">
+            <em className={`absolute block h-7 w-7 top-1/2 left-7 transition-transform duration-200 ease-out delay-200
+            before:-rotate-45 before:absolute before:left-0 before:h-full before:w-1 before:bottom-[1px] before:bg-black before:origin-top before:duration-200 before:ease-out
+            after:-rotate-[135deg] after:absolute after:left-0 after:h-full after:w-1 after:bg-black after:origin-top after:duration-300 after:ease-out
+            group-hover:before:-rotate-[25deg] group-hover:after:-rotate-[155deg]
+            ${arrowL ? "before:animate-leftDown before:bottom-0 after:animate-leftUp" : ""}`}
+            />
+          </div>
         </button>
         <button
-          className={`absolute block w-1/4 h-full z-20 right-0 bg-gradient-to-r from-transparent to-slate-700 transition-opacity duration-700 ease-out opacity-[${right ? 0.5 : 0.1}]`}
+          className={`absolute group block w-1/4 h-full z-20 right-0`}
           onClick={next}
-          onMouseEnter={() => setRight(true)}
-          onMouseLeave={() => setRight(false)}
         >
-          <em className={`absolute block h-7 w-7 top-1/2 right-7 transition-transform duration-200 ease-out delay-200
-          before:rotate-${right ? "[25deg]" : 45} before:absolute before:right-0 before:h-full before:w-1 before:bottom-[1px] before:bg-black before:origin-top before:duration-200 before:ease-out
-          after:rotate-${right ? "[155deg]" : "[135deg]"} after:absolute after:right-0 after:h-full after:w-1 after:bg-black after:origin-top after:duration-300 after:ease-out
-          ${arrowR ? "before:animate-rightDown before:bottom-0 after:animate-rightUp" : ""}`}
-          />
+          <div className="w-full h-full bg-gradient-to-r from-transparent to-slate-700 transition-opacity duration-500 ease-out opacity-10 group-hover:opacity-50">
+            <em className={`absolute block h-7 w-7 top-1/2 right-7 transition-transform duration-200 ease-out delay-200
+            before:rotate-45 before:absolute before:right-0 before:h-full before:w-1 before:bottom-[1px] before:bg-black before:origin-top before:duration-200 before:ease-out
+            after:rotate-[135deg] after:absolute after:right-0 after:h-full after:w-1 after:bg-black after:origin-top after:duration-300 after:ease-out
+            group-hover:before:rotate-[25deg] group-hover:after:rotate-[155deg]
+            ${arrowR ? "before:animate-rightDown before:bottom-0 after:animate-rightUp" : ""}`}
+            />
+          </div>
         </button>
         <AnimatePresence initial={false} custom={direction}>
           { phone ?
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -11,8 +11,6 @@ const slides = [ insta, portfolio, dpower, pokemon ];
 
 export default function Projects() {
 
-  const [left, setLeft] = useState(false);
-  const [right, setRight] = useState(false);
   const [arrowL, setArrowL] = useState(false);
   const [arrowR, setArrowR] = useState(false);
   const [curr, setCurr] = useState(0);
@@ -74,25 +72,23 @@ export default function Projects() {
 
           <button
             onClick={prev}
-            className="hidden absolute h-full w-[70px] lg:block"
-            onMouseEnter={() => setLeft(true)}
-            onMouseLeave={() => setLeft(false)}
+            className="hidden absolute h-full w-[70px] group lg:block"
           >
             <em className={`absolute block h-8 w-8 top-1/2 left-3 transition-transform duration-200 ease-out delay-200 xl:left-5 xl:h-9 xl:w-9
-            before:-rotate-${left ? "[25deg]" : 45} before:absolute before:left-0 before:h-full before:w-[5px] before:bg-slate-700 before:dark:bg-[#dfdfdf] before:origin-top-left before:duration-200 before:ease-out
-            after:-rotate-${left ? "[155deg]" : "[135deg]"} after:absolute after:-left-[5px] after:h-full after:w-[5px] after:bg-slate-700 after:dark:bg-[#dfdfdf] after:origin-top-right after:duration-200 after:ease-out
+            before:-rotate-45 before:absolute before:left-0 before:h-full before:w-[5px] before:bg-slate-700 before:dark:bg-[#dfdfdf] before:origin-top-left before:duration-200 before:ease-out
+            after:-rotate-[135deg] after:absolute after:-left-[5px] after:h-full after:w-[5px] after:bg-slate-700 after:dark:bg-[#dfdfdf] after:origin-top-right after:duration-200 after:ease-out
+            group-hover:before:-rotate-[25deg] group-hover:after:-rotate-[155deg]
             ${arrowL ? "before:animate-leftDown after:bottom-[5px] after:animate-leftUp" : ""}`}
             />
           </button>
           <button
             onClick={next}
-            className="hidden absolute right-0 h-full w-[70px] lg:block"
-            onMouseEnter={() => setRight(true)}
-            onMouseLeave={() => setRight(false)}
+            className="hidden absolute right-0 h-full w-[70px] group lg:block"
           >
             <em className={`absolute block h-8 w-8 top-1/2 right-3 transition-transform duration-200 ease-out delay-200 xl:right-5 xl:h-9 xl:w-9
-            before:rotate-${right ? "[25deg]" : 45} before:absolute before:right-0 before:h-full before:w-[5px] before:bg-slate-700 before:dark:bg-[#dfdfdf] before:origin-top-right before:duration-200 before:ease-out
-            after:rotate-${right ? "[155deg]" : "[135deg]"} after:absolute after:-right-[5px] after:h-full after:w-[5px] after:bg-slate-700 after:dark:bg-[#dfdfdf] after:origin-top-left after:duration-200 after:ease-out
+            before:rotate-45 before:absolute before:right-0 before:h-full before:w-[5px] before:bg-slate-700 before:dark:bg-[#dfdfdf] before:origin-top-right before:duration-200 before:ease-out
+            after:rotate-[135deg] after:absolute after:-right-[5px] after:h-full after:w-[5px] after:bg-slate-700 after:dark:bg-[#dfdfdf] after:origin-top-left after:duration-200 after:ease-out
+            group-hover:before:rotate-[25deg] group-hover:after:rotate-[155deg]
             ${arrowR ? "before:animate-rightDown after:bottom-[5px] after:animate-rightUp" : ""}`}
             />
           </button>
