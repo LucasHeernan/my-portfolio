7d57675c9ab4f9d928bad3926292b7e3e33237f8
LucasHeernan <lhbenitez2@gmail.com>
2023-12-28 01:31:07 -0300

    Update ProjectItem

--- src/components/Projects/ProjectItem.jsx
+++ src/components/Projects/ProjectItem.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import ProjectImage from "./ProjectImage";
 import ProjectNavBar from "./ProjectNavBar";
 import ProjectFooter from "./ProjectFooter";
@@ -25,23 +25,7 @@ const techAnimations = {
 export default function ProjectItem() {
 
   const { id } = useParams();
-  const [project, setProject] = useState(null);
-  
-  useEffect(() => {
-    setProject(projectsData.find((project) => project.id === Number(id)));
-    const fetchData = async () => {
-      try {
-        console.log("ESTADO PROJECT --> ", project);
-        const projectInfo = projectsData.find((p) => p.id === Number(id));
-        await new Promise((res) => setTimeout(res, 0));
-        setProject(projectInfo);
-        console.log("ESTADO PROJECT POST SET --> ", project);
-      } catch (error) {
-        console.error("Error al obtener la información del proyecto", error);
-      }
-    };
-    fetchData();
-  }, [id]);
+  const project = projectsData.find((project) => project.id === Number(id));
 
   useEffect(() => {
     setTimeout(() => {
@@ -52,89 +36,84 @@ export default function ProjectItem() {
   return (
     <div className="w-full cursor-default font-['Montserrat'] transition-colors duration-75 ease-in-out bg-white dark:bg-[#2a2a2a]">
       <ProjectNavBar />
-      <div className="max-w-7xl w-full flex flex-col mx-auto pb-10 px-5 xs:px-10 sm:px-16 md:px-20 text-[#2a2a2a] dark:text-[#8a8a8a]">
-        {
-          !project ? <div className="w-full h-screen bg-green-500 flex items-center justify-center"><h2 className="text-5xl font-extrabold">CARGADO...</h2></div> :
-          <>
-            <section className="flex md:flex-row animate-fadeIn">
-              <div className="relative z-0 pr-5 mt-8 mb-3 xs:pr-8 sm:mt-10">
-                <h2 className="text-2xl font-bold tracking-wider uppercase scale-y-110 md:scale-y-100 xs:text-[27px] md:text-4xl text-[#2a2a2a] dark:text-[#cacaca]">{project.title}</h2>
-                <h2 className="text-2xl font-bold tracking-wider uppercase scale-y-110 md:scale-y-100 xs:text-[27px] md:text-4xl absolute text-[rgba(0,0,0,.20)] top-0.5 left-0.5 dark:top-[3px] dark:left-[3px] -z-10">{project.title}</h2>
-              </div>
+      <div className="max-w-7xl w-full flex flex-col mx-auto pb-10 px-5 animate-fadeIn xs:px-10 sm:px-16 md:px-20 text-[#2a2a2a] dark:text-[#8a8a8a]">
+        <section className="flex md:flex-row">
+          <div className="relative z-0 pr-5 mt-8 mb-3 xs:pr-8 sm:mt-10">
+            <h2 className="text-2xl font-bold tracking-wider uppercase scale-y-110 md:scale-y-100 xs:text-[27px] md:text-4xl text-[#2a2a2a] dark:text-[#cacaca]">{project.title}</h2>
+            <h2 className="text-2xl font-bold tracking-wider uppercase scale-y-110 md:scale-y-100 xs:text-[27px] md:text-4xl absolute text-[rgba(0,0,0,.20)] top-0.5 left-0.5 dark:top-[3px] dark:left-[3px] -z-10">{project.title}</h2>
+          </div>
 
-              <div className="flex items-center pt-5 sm:pt-7">
-                <div className="relative z-10">
+          <div className="flex items-center pt-5 sm:pt-7">
+            <div className="relative z-10">
+              <a
+                href={`${project.repository}`}
+                target="_blank"
+                className="w-9 h-9 flex justify-center items-center rounded-full bg-gradient-to-tl from-slate-800 to-slate-500 border-b-2 border-r border-slate-900 shadow-sm shadow-black"
+              >
+                <Github fill={"white"} className="w-6" />
+              </a>
+              <span className="absolute w-[calc(100%+1px)] h-[calc(100%+1px)] -z-10 top-px left-px rounded-full bg-[rgb(0,0,0,.70)] dark:bg-[#1d1c1c]" />
+            </div>
+
+            {
+              project.youtube ?
+                <div className="relative z-10 ml-5">
                   <a
-                    href={`${project.repository}`}
+                    href={`${project.youtube}`}
                     target="_blank"
-                    className="w-9 h-9 flex justify-center items-center rounded-full bg-gradient-to-tl from-slate-800 to-slate-500 border-b-2 border-r border-slate-900 shadow-sm shadow-black"
+                    className="w-9 h-9 flex justify-center items-center rounded-full bg-[#ce1312] border-b-2 border-r border-[rgb(159,18,57)] shadow-sm shadow-black"
                   >
-                    <Github fill={"white"} className="w-6" />
+                    <Youtube fill={"white"} className="w-[22px] ml-px mt-px" />
                   </a>
-                  <span className="absolute w-[calc(100%+1px)] h-[calc(100%+1px)] -z-10 top-px left-px rounded-full bg-[rgb(0,0,0,.70)] dark:bg-[#1d1c1c]" />
+                  <span className="absolute w-[calc(100%+1px)] h-[calc(100%+1px)] -z-10 top-px left-px rounded-full bg-[rgb(0,0,0,.70)] dark:bg-[#1d1c1c]"/>
                 </div>
+              : null
+            }
+          </div>
+          
+        </section>
 
-                {
-                  project.youtube ?
-                    <div className="relative z-10 ml-5">
-                      <a
-                        href={`${project.youtube}`}
-                        target="_blank"
-                        className="w-9 h-9 flex justify-center items-center rounded-full bg-[#ce1312] border-b-2 border-r border-[rgb(159,18,57)] shadow-sm shadow-black"
-                      >
-                        <Youtube fill={"white"} className="w-[22px] ml-px mt-px" />
-                      </a>
-                      <span className="absolute w-[calc(100%+1px)] h-[calc(100%+1px)] -z-10 top-px left-px rounded-full bg-[rgb(0,0,0,.70)] dark:bg-[#1d1c1c]"/>
-                    </div>
-                  : null
-                }
-              </div>
-              
-            </section>
+        <p className="text-base font-medium mb-7 pr-5 md:text-lg md:pr-10 lg:pr-32">{project.subtitle}</p>
 
-            <p className="text-base font-medium mb-7 pr-5 animate-fadeIn md:text-lg md:pr-10 lg:pr-32">{project.subtitle}</p>
+        <section className={`flex flex-col items-center ${project.phone ? "lg:h-[600px] lg:flex lg:flex-row lg:items-stretch" : "xl:h-[380px] xl:flex xl:flex-row xl:items-stretch"}`}>
 
-            <section className={`flex flex-col items-center animate-fadeIn ${project.phone ? "lg:h-[600px] lg:flex lg:flex-row lg:items-stretch" : "xl:h-[380px] xl:flex xl:flex-row xl:items-stretch"}`}>
+          <ProjectImage images={project.images} phone={project.phone} id={project.id} />
 
-              <ProjectImage images={project.images} phone={project.phone} id={project.id} />
-
-              <div className={`w-full flex flex-col text-left ${project.phone ? "lg:w-[55%] lg:pl-20 lg:justify-around" : "xl:w-1/2 xl:pl-10 xl:justify-evenly"}`}>
-                <h3 className="text-3xl font-bold tracking-wide mb-2 xl:mb-0 text-[#2a2a2a] dark:text-[#cacaca]">Sobre este proyecto</h3>
-                <div className="text-base md:text-lg font-medium">
-                  <p>{project.description}</p>
-                  <p>{project.description2}</p>
-                </div>
-                <div className="flex flex-col">
-                  <h3 className="text-3xl font-bold tracking-wide py-3 xl:py-0 xl:pb-3 text-[#2a2a2a] dark:text-[#cacaca]">Tecnologías</h3>
-                  <div className="relative">
-                    <div className="absolute">
-                      {
-                        project.techs.map((el, index) => {
-                          return (
-                            <motion.div
-                              key={index}
-                              variants={techAnimations}
-                              initial="initial"
-                              whileInView="animate"
-                              custom={index}
-                              viewport={{ once: true }}
-                              className="inline-block p-1 bg-[#313131] text-[#999] border-solid border-t-[1px] border-r-2 border-b-[3px] border-l-[1px] border-[#222] rounded-sm uppercase font-semibold text-sm shadow-sm shadow-[rgba(0,0,0,.25)] mr-3 md:mr-4 relative overflow-hidden box-border
-                              before:bg-[#515151] before:top-0 before:left-0 before:w-0 before:h-1/2 hover:before:w-full before:block before:absolute before:z-10 before:duration-200 before:ease-in-out
-                              after:bg-[#3f3f3f] after:bottom-0 after:right-0 after:w-0 after:h-1/2 hover:after:w-full after:block after:absolute after:z-10 after:transition-all after:duration-200 after:ease-in-out after:delay-100"
-                            >
-                              <span className="relative z-20 font-poppins">{el}</span>
-                            </motion.div>
-                          )
-                        })
-                      }
-                    </div>
-                  </div>
+          <div className={`w-full flex flex-col text-left ${project.phone ? "lg:w-[55%] lg:pl-20 lg:justify-around" : "xl:w-1/2 xl:pl-10 xl:justify-evenly"}`}>
+            <h3 className="text-3xl font-bold tracking-wide mb-2 xl:mb-0 text-[#2a2a2a] dark:text-[#cacaca]">Sobre este proyecto</h3>
+            <div className="text-base md:text-lg font-medium">
+              <p>{project.description}</p>
+              <p>{project.description2}</p>
+            </div>
+            <div className="flex flex-col">
+              <h3 className="text-3xl font-bold tracking-wide py-3 xl:py-0 xl:pb-3 text-[#2a2a2a] dark:text-[#cacaca]">Tecnologías</h3>
+              <div className="relative">
+                <div className="absolute">
+                  {
+                    project.techs.map((el, index) => {
+                      return (
+                        <motion.div
+                          key={index}
+                          variants={techAnimations}
+                          initial="initial"
+                          whileInView="animate"
+                          custom={index}
+                          viewport={{ once: true }}
+                          className="inline-block p-1 bg-[#313131] text-[#999] border-solid border-t-[1px] border-r-2 border-b-[3px] border-l-[1px] border-[#222] rounded-sm uppercase font-semibold text-sm shadow-sm shadow-[rgba(0,0,0,.25)] mr-3 md:mr-4 relative overflow-hidden box-border
+                          before:bg-[#515151] before:top-0 before:left-0 before:w-0 before:h-1/2 hover:before:w-full before:block before:absolute before:z-10 before:duration-200 before:ease-in-out
+                          after:bg-[#3f3f3f] after:bottom-0 after:right-0 after:w-0 after:h-1/2 hover:after:w-full after:block after:absolute after:z-10 after:transition-all after:duration-200 after:ease-in-out after:delay-100"
+                        >
+                          <span className="relative z-20 font-poppins">{el}</span>
+                        </motion.div>
+                      )
+                    })
+                  }
                 </div>
               </div>
+            </div>
+          </div>
 
-            </section>
-          </>
-        }
+        </section>
         <ProjectFooter />
       </div>
     </div>
